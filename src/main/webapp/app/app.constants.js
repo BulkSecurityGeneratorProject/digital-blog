feb18324bf919d5319692fef834e4e6a4b904a32
Theseeker <jgm16@hotmail.com>
2017-09-27 02:03:28 -0600

    asd3

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('digitalBlogApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1506485501257)
+        .constant('BUILD_TIMESTAMP', 1506498805700)
 ;
 })();
