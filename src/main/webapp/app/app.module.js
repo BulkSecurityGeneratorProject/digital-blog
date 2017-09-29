5dc95534d16329843ecc2c862ac0b7201c83cd6b
Theseeker <jgm16@hotmail.com>
2017-09-29 14:15:50 -0600

    no message

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -18,6 +18,7 @@
             'ui.tinymce',
             'infinite-scroll',
             'toastr',
+            'autocomplete',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar',
             'angular-notification-icons'
