5ea06f07650298e5ca77bda19f27b57303fcb369
Theseeker <jgm16@hotmail.com>
2017-09-29 13:54:56 -0600

    ngAnimate, and sanitize on module

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -9,6 +9,8 @@
             'ngAria',
             'ngCacheBuster',
             'ngFileUpload',
+            'ngAnimate',
+            'ngSanitize',
             'ui.bootstrap',
             'ui.bootstrap.datetimepicker',
             'ui.bootstrap.contextMenu',
@@ -19,8 +21,6 @@
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar',
             'angular-notification-icons'
-
-
 
 
         ])
